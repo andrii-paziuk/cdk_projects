4c430c5487a7646d6ef89a59455a2f365fd6006f
Andrii Paziuk <andrii.paziuk@gmail.com>
2022-09-26 14:12:00 +0200

    Initial commit, creating S3 bucker

---
+++ CDK_S3/test/cdk_s3.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as CdkS3 from '../lib/cdk_s3-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_s3-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkS3.CdkS3Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

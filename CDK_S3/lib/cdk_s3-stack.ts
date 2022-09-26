d465c5e0607016f62c48f244d78f86ee8d278a56
Andrii Paziuk <andrii.paziuk@gmail.com>
2022-09-26 14:45:53 +0200

    removing typo

--- CDK_S3/lib/cdk_s3-stack.ts
+++ CDK_S3/lib/cdk_s3-stack.ts
@@ -1,7 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 // import * as sqs from '@aws-cdk/aws-sqs';
 
-export class CdkS3Stack extends cdk.Stack {
+export class S3Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 

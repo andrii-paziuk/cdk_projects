a7e38c480314bb5183870a1007959fdeb5e8255e
Andrii Paziuk <andrii.paziuk@gmail.com>
2022-09-26 14:46:24 +0200

    Removing typo

--- CDK_S3/bin/cdk_s3.ts
+++ CDK_S3/bin/cdk_s3.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { CdkS3Stack } from '../lib/cdk_s3-stack';
+import { S3Stack } from '../lib/cdk_s3-stack';
 
 const app = new cdk.App();
-new CdkS3Stack(app, 'CdkS3Stack', {
+new S3Stack(app, 'S3Stack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
